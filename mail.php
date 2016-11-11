5c7a311dd1a42bf38210a18c0bfb30253bf82811
rachelmharrison <rachelmharrison8@gmail.com>
2016-11-11 16:24:28 -0500

    additional testing

--- mail.php
+++ mail.php
@@ -1,6 +1,7 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+
+<?php $name = $_GET['name'];
+$email = $_GET['email'];
+$message = $_GET['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "rharr086@uottawa.ca";
 $subject = "Contact Form";
